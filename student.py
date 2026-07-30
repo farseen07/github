e8fff47945211a8454c34f83bec31e17ebbf6b82
farseen <chuhefarseen@gmail.com>
2026-07-30 10:34:52 +0530

    latest

--- student.py
+++ student.py
@@ -1,18 +1,4 @@
 class Student: 
-    def __init__(self, name, age, grade):
-        self.name = name
-        self.age = age
-        self.grade = grade
+    print("This is a student class")
 
-    def get_info(self):
-        return f"Name: {self.name}, Age: {self.age}, Grade: {self.grade}"
-
-    def is_passing(self):
-        return self.grade >= 60
-    class teacher:
-        def __init__(self, name, subject):
-            self.name = name
-            self.subject = subject
-
-        def get_info(self):
-            return f"Teacher Name: {self.name}, Subject: {self.subject}"
+    print("saqib dogla")
